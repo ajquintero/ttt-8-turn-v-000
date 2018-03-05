0a6af45073797121777799bf8e8c30340c1a854e
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-05 22:32:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,4 @@
 def move(board, index, token = "X")
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
   def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -8,19 +7,20 @@ def move(board, index, token = "X")
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
-  def valid_move?(board, index)
-    if position_taken?(board, index) == false && index.between?(0,8) == true
-      return true
-    else
-      return false
-    end
-  end
-  def position_taken?(board, index)
-    if board[index] == " " || board[index] == "" || board[index] == nil
-      return false
-    elsif board[index] == "X" || board[index] == "O"
-    return true
-    end
-  end
+
+  # def valid_move?(board, index)
+  #   if position_taken?(board, index) == false && index.between?(0,8) == true
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+  # def position_taken?(board, index)
+  #   if board[index] == " " || board[index] == "" || board[index] == nil
+  #     return false
+  #   elsif board[index] == "X" || board[index] == "O"
+  #   return true
+  #   end
+  # end
 
 end
